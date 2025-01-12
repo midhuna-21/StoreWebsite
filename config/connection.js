8e590aab46b1651ac9a55864d236fe11b9ac8bb2
Mdhuna r <rmidhuna4@gmail.com>
2025-01-12 14:03:05 +0530

    mogno uri changed

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const connectDB = () =>{
-    return mongoose.connect('mongodb+srv://midhunaradhakrishnan31:idMnKyOFd2lMfcPn@cluster0.wg0fa.mongodb.net/', {
-        useNewUrlParser : true,
-        useUnifiedTopology: true,
-    })
-    .then(() =>{
-        console.log("mongodb connected");
-    })
-    .catch((err) =>{
-        console.log("monogodb connection error:", err);
-    })
-}
+
 
 module.exports = connectDB;
 
