28777fcadbbccb79543ab27838f9de9290cb77f8
Mdhuna r <rmidhuna4@gmail.com>
2025-01-12 14:25:00 +0530

    mogno uri changed3

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ const connectDB = () =>{
     return mongoose.connect('mongodb+srv://midhunaradhakrishnan31:idMnKyOFd2lMfcPn@cluster0.wg0fa.mongodb.net/', {
         useNewUrlParser : true,
         useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000,
     })
     .then(() =>{
         console.log("mongodb connected");
